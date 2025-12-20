38da216409003d0740aa2b9a142297bf2e5d1ee7
Moviifox <moviifox.web@gmail.com>
2025-12-20 20:10:09 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -567,7 +567,7 @@ const GridMovieCard = ({ movie, isFocused, onClick, innerRef }) => {
          {/* Badge on Image */}
          <div className="absolute top-[0.8vw] right-[0.8vw] flex gap-[0.3vw]">
             {movie.type && (
-               <span className="bg-black/40 backdrop-blur-md text-white/90 text-[0.7vw] px-[0.5vw] py-[0.15vw] rounded-md border border-white/10 flex items-center gap-1 font-light uppercase tracking-wider">
+               <span className="bg-black/40 backdrop-blur-md text-white/90 text-[1vw] px-[0.5vw] py-[0.15vw] rounded-[0.8vw] border border-white/10 flex items-center gap-1 font-light uppercase tracking-wider">
                {movie.type}
                </span>
             )}
