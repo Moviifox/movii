63ccab21bd6803a632f6a6f110b155b954dbd56e
Moviifox <moviifox.web@gmail.com>
2026-04-10 20:57:28 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1900,7 +1900,7 @@ const Modal = ({ movie, onClose }) => {
 
           {/* Description truncated + more button */}
           <div className="text-zinc-300 text-[1.6vw] font-regular leading-snug mb-[2.6vw] font-light mix-blend-plus-lighter">
-            <div ref={descRef} className="line-clamp-4">{movie.description || FEATURED_MOVIE.description}</div>
+            <div ref={descRef} className="line-clamp-3">{movie.description || FEATURED_MOVIE.description}</div>
             {movie.description && hasMoreDesc && (
               <button
                 type="button"
